2da946e753506b1ba673849341d629649239e70c
Codezzzy <codydavis694@gmail.com>
2025-01-18 22:56:05 +0000

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["#BISTRIEZ", "Chairperson: dsxiac", "Co-Holder(s): glossifieds", "Co-Holder(s): FlyAway921"];
+const statusMessages = ["#BISTRIEZ", "Holder: dsxiac", "Co-Holder(s): glossifieds", "Co-Holder(s): FlyAway921"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
